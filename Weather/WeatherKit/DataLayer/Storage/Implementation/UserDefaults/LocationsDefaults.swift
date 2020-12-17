25b4c362bc178cf89e72dce7507df346b8c340b4
Sangeui <Sangeuis@icloud.com>
2020-12-17 18:58:06 +0900

    commit

---
+++ Weather/WeatherKit/DataLayer/Storage/Implementation/UserDefaults/LocationsDefaults.swift
@@ -0,0 +1,20 @@
+//
+//  UnitDefaults.swift
+//  WeatherKit
+//
+//  Created by 서상의 on 2020/12/17.
+//
+
+import Foundation
+
+struct LocationsDefaults {
+    let key: Key
+    let defaults: UserDefaults
+    
+    var value: UserSavedLocations? {
+        get {
+        }
+        set {
+        }
+    }
+}
