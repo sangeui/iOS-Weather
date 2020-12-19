d28b759d84f25acd70ce25bd90573226a912b786
Sangeui <Sangeuis@icloud.com>
2020-12-19 19:40:06 +0900

    added NetworkSession extension for URLSession

--- Weather/WeatherKit/DataLayer/Network/NetworkSession/Implementation/URLSession+NetworkSession.swift
+++ Weather/WeatherKit/DataLayer/Network/NetworkSession/Implementation/URLSession+NetworkSession.swift
@@ -6,8 +6,18 @@
 //
 
 import Foundation
-
 extension URLSession: NetworkSession {
     func execute<T>(_ url: URL, completion: @escaping (Result<T, Error>) -> Void) where T : Decodable {
+        let task = dataTask(with: url, completionHandler: { [weak self] (data, response, error) in
+            guard let weakSelf = self else { return }
+            if let error = error { completion(.failure(error)) }
+            guard let data = data else { return }
+            guard let decoded = weakSelf.json(T.self, from: data) else { return }
+            completion(.success(decoded))
+        })
+        task.resume()
+    }
+    private func json<T: Decodable>(_ type: T.Type, from data: JSONDecoder.Input) -> T? {
+        return try? jsonCamelDecoder.decode(type.self, from: data)
     }
 }
--- Weather/WeatherKit/DataLayer/Network/NetworkSession/NetworkSession.swift
+++ Weather/WeatherKit/DataLayer/Network/NetworkSession/NetworkSession.swift
@@ -7,6 +7,18 @@
 
 import Foundation
 
+typealias NetworkCompletion<T: Decodable> = (Result<T, Error>) -> Void
+
 protocol NetworkSession {
-    func execute<T: Decodable>(_ url: URL, completion: @escaping (Result<T, Error>) -> Void)
+    func execute<T: Decodable>(_ url: URL, completion: @escaping NetworkCompletion<T>)
+}
+extension NetworkSession {
+    var jsonEncoder: JSONEncoder { return JSONEncoder() }
+    var jsonDecoder: JSONDecoder { return JSONDecoder() }
+    
+    var jsonCamelDecoder: JSONDecoder {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }
 }
