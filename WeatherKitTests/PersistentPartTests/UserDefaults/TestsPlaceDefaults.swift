959160f4af257ac18871c9dbb7bc61c15e02d778
Sangeui <Sangeuis@icloud.com>
2020-12-24 00:10:45 +0900

    added PlaceDefaults tests

--- WeatherKitTests/PersistentPartTests/UserDefaults/TestsPlaceDefaults.swift
+++ WeatherKitTests/PersistentPartTests/UserDefaults/TestsPlaceDefaults.swift
@@ -20,9 +20,42 @@ class TestsPlaceDefaults: XCTestCase {
         placeDefaults = nil
     }
     func testSetAndGetPlaces() {
+        let place = UniquePlace.randomPlace
+        let timestamp = String(NSDate().timeIntervalSince1970)
+        let uniquePlace = UniquePlace(place: place, timestamp: timestamp)
         
+        XCTAssertNil(placeDefaults.value)
+        
+        placeDefaults.value = [uniquePlace]
+        
+        let savedPlace = placeDefaults.value
+        
+        XCTAssertNotNil(savedPlace)
+        XCTAssertEqual(savedPlace!.count, 1)
+        XCTAssertEqual(savedPlace!, [uniquePlace])
+    }
+    func testNothingHappendWhenPassedNil() {
+        let place = UniquePlace.randomPlace
+        let timestamp = String(NSDate().timeIntervalSince1970)
+        let uniquePlace = UniquePlace(place: place, timestamp: timestamp)
+        
+        placeDefaults.value = [uniquePlace]
+        placeDefaults.value = nil
+        
+        let savedPlace = placeDefaults.value
+        
+        XCTAssertNotNil(savedPlace)
+        XCTAssertEqual(savedPlace!, [uniquePlace])
     }
 }
 fileprivate extension UniquePlace {
-    static var makePlace
+    static private var randomCoordinate: Coordinate {
+        let latitude = Double.random(in: -90...90)
+        let longitude = Double.random(in: -180...180)
+        return Coordinate(latitude: latitude, longitude: longitude)
+    }
+    static var randomPlace: Place {
+        let words = ["A", "B", "C", "D", "E", "F", "G"]
+        return Place(coordination: randomCoordinate, name: words.randomElement()!)
+    }
 }
