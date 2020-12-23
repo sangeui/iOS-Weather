47694e73e9467e51890a9aa8bcd2fe45dee9fea6
Sangeui <Sangeuis@icloud.com>
2020-12-24 02:41:26 +0900

    added new location protocol.

---
+++ WeatherKit/CoreServiceLayer/LocationPart/Implementation/CLLocationManager+LocationProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  CLLocationManager+LocationProtocol.swift
+//  WeatherKit
+//
+//  Created by 서상의 on 2020/12/24.
+//
+
+import CoreLocation
--- WeatherKit/CoreServiceLayer/LocationPart/Protocol/LocationProtocol.swift
+++ WeatherKit/CoreServiceLayer/LocationPart/Protocol/LocationProtocol.swift
@@ -5,10 +5,52 @@
 //  Created by 서상의 on 2020/12/21.
 //
 
-import Foundation
+import CoreLocation
 
 typealias LocationHandler = ((Result<Coordinate, LocationError>) -> Void)
 
 protocol LocationProtocol {
     func getLocation(completion: @escaping LocationHandler)
+}
+
+protocol LocationManager {
+    // CLLocationManager Properties
+    var location: CLLocation? { get }
+    var delegate: CLLocationManagerDelegate? { get set }
+    
+    // CLLocationManager Methods
+    func requestWhenInUseAuthorization()
+    func startUpdatingLocation()
+    func stopUpdatingLocation()
+    
+    // Wrappers for CLLocationManager class functions.
+    func getAuthorizationStatus() -> CLAuthorizationStatus
+    func isLocationServicesEnabled() -> Bool
+}
+extension CLLocationManager: LocationManager {
+    func isLocationServicesEnabled() -> Bool {
+        return CLLocationManager.locationServicesEnabled()
+    }
+    func getAuthorizationStatus() -> CLAuthorizationStatus {
+        return self.authorizationStatus
+    }
+}
+
+class MockLocationManager: LocationManager {
+    var location: CLLocation? = CLLocation(
+        latitude: 37.3317,
+        longitude: -122.0325086)
+    
+    var delegate: CLLocationManagerDelegate?
+    
+    func requestWhenInUseAuthorization() { }
+    func startUpdatingLocation() { }
+    func stopUpdatingLocation() { }
+    
+    func getAuthorizationStatus() -> CLAuthorizationStatus {
+        return .authorizedWhenInUse
+    }
+    func isLocationServicesEnabled() -> Bool {
+        return true
+    }
 }
